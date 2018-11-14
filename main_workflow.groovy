97cfedd6ca06cfe102e53dcc9d0ef784c81d2f59
arie.belenky <ariebelenky@Aries-MacBook-Pro.local>
2018-11-14 15:28:19 +0200

    added repo stage

--- main_workflow.groovy
+++ main_workflow.groovy
@@ -15,6 +15,10 @@ parallel(
 
                 }
 
+                stage('Create local docker repo'){
+                    sh 'docker run -d -p 5000:5000 --restart=always --name registry registry:2'
+                }
+
                 stage('Build'){
                     sh 'sudo docker build --no-cache -t localhost:5000/opsschool_dummy_app:latest .'
                     sh 'sudo docker push localhost:5000/opsschool_dummy_app:latest'
