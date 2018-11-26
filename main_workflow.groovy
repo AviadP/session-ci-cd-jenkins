bf1f12d5c5cec8b3e1ce6307f7a8c9d471796eb6
arie.belenky <ariebelenky@Aries-MacBook-Pro.local>
2018-11-26 13:28:42 +0200

    added test 10

--- main_workflow.groovy
+++ main_workflow.groovy
@@ -13,12 +13,14 @@ parallel(
                 }
 
                 stage('Unit Tests'){
-                    sh 'pip install -r python_app/requirements.txt'
-                    sh 'python -m virtualenv --system-site-packages NEW_ENV'
-                    sh '. ./NEW_ENV/bin/activate'
-                    sh 'pip install -r python_app/requirements.txt'
-                    sh 'python -m pytest --junitxml results.xml python_app/tests/test_server.py'
-                    sh '. ./deactivate'
+                    bash '''#!/bin/bash
+                            pip install -r python_app/requirements.txt
+                            python -m virtualenv --system-site-packages NEW_ENV
+                            source NEW_ENV/bin/activate
+                            pip install -r python_app/requirements.txt
+                            python -m pytest --junitxml results.xml python_app/tests/test_server.py
+                            deactivate
+                     '''
 
                 }
 
